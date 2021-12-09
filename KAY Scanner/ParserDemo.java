87bc7bca04070e02118198fdceb8eeaeacdbeb84
edricjchai <53452387+edricjchai@users.noreply.github.com>
2021-12-09 02:41:37 -0500

    Parse upload

---
+++ KAY Scanner/ParserDemo.java
@@ -0,0 +1,15 @@
+public class ParserDemo {
+
+	public static void main(String[] args) {
+
+		// TO COMPLETE
+		// Change the path!
+		TokenStream tStream = new TokenStream("C:\\Users\\Admin\\IdeaProjects\\KAYScanner\\src\\prog1.jay");
+
+		ConcreteSyntax cSyntax = new ConcreteSyntax(tStream);
+		Program p = cSyntax.program();
+		System.out.println(p.display());
+		System.out.println("test");
+	}
+
+}
